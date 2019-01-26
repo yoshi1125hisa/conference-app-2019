0a3c5b4839fa56d73c656a09946e71f8f4799e40
Jumpei Matsuda <jmatsu.drm@gmail.com>
2019-01-26 20:45:41 +0900

    Dirty hack

--- .travis/ios/before_install.bash
+++ .travis/ios/before_install.bash
@@ -7,4 +7,19 @@ cd frontend/ios
 bundle install
 make init
 
+brew tap caskroom/versions
+brew cask install java8
+
+# switch jdk to 1.8 anyway
+cp ./gradlew gradlew.origin
+
+cat<<'EOF' > ./gradlew
+#!/usr/bin/env bash
+
+export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
+./gradlew.origin "$@"
+EOF
+
+chmod +x ./gradlew
+
 ./gradlew androidDependenciesExtra getDependencies
